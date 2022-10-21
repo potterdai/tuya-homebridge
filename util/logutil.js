a9a513d2def565af2b32aac303d0fd04cc8efd1c
Potter Dai <potterdaica@gmail.com>
2022-10-21 13:48:59 -0700

    Fix.

--- util/logutil.js
+++ util/logutil.js
@@ -5,15 +5,19 @@ class LogUtil {
 
     log(...args) {
         if (this.isDebug) {
-            console.log(...args);
+            console.log(...prependTimestamp(args));
         }
     }
 
     error(...args) {
         if (this.isDebug) {
-            console.log(...args);
+            console.log(...prependTimestamp(args));
         }
     }
 }
+
+function prependTimestamp(args) {
+    return ['[' + (new Date()).toISOString() + ']', ...args];
+};
 
 module.exports = LogUtil;
